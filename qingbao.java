784bb5d316640d9d396c89d92e5c7b0fec2681c7
skiller <842636667@qq.com>
2015-03-21 11:14:47 +0800

    参考
    里面有要用到的相关软件以及工程的说明和教程，大家看一下，基本都会用到

---
+++ qingbao.java
@@ -0,0 +1,10 @@
+
+SOLR£ºhttp://blog.csdn.net/awj3584/article/details/16963525
+
+Standford CoreNLP£ºhttp://www.cnblogs.com/loveyakamoz/archive/2011/11/30/2269509.html
+
+
+WEKA£ºhttp://blog.csdn.net/yangliuy/article/details/7589306
+
+
+Apache Mahout£ºhttp://www.ibm.com/developerworks/cn/java/j-mahout/
